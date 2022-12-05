5437e2c09f975574831a2bd58675f661540b1824
MA JIAHAO <63329206+Ascaris-Equi@users.noreply.github.com>
2022-12-05 20:21:02 +0800

    Update fuctions.py

--- fuctions.py
+++ fuctions.py
@@ -3,6 +3,17 @@ import matplotlib.pyplot as plt
 import itertools
 import functools
 
+def Persistent_Homology(data,epsilon,k):
+  graph = buildGraph(raw_data=data, epsilon=epsilon) #epsilon = 9 will build a "maximal complex"
+  ripsComplex = ripsFiltration(graph, k=k)
+  # drawComplex(origData=data, ripsComplex=ripsComplex[0])
+  BoundaryMatrix = filterBoundaryMatrix(ripsComplex)
+  Reducced_BoundaryMatrix = reduceBoundaryMatrix(BoundaryMatrix)
+  intervals = readIntervals(Reducced_BoundaryMatrix, ripsComplex[1])
+  persist = readPersistence(intervals, ripsComplex)
+  graph_barcode(persist, 0)
+  graph_barcode(persist, 1)
+
 def euclidianDist(a,b):
     return np.linalg.norm(a - b) #euclidian distance metric
 
