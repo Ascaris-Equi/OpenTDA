fc7898eabe845b768a5f887eda28926a8cf42bd3
MA JIAHAO <63329206+Ascaris-Equi@users.noreply.github.com>
2022-12-05 20:16:32 +0800

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,7 @@
 def Persistent_Homology(data,epsilon,k):
   graph = buildGraph(raw_data=data, epsilon=epsilon) #epsilon = 9 will build a "maximal complex"
   ripsComplex = ripsFiltration(graph, k=k)
-  drawComplex(origData=data, ripsComplex=ripsComplex[0])
+  # drawComplex(origData=data, ripsComplex=ripsComplex[0])
   BoundaryMatrix = filterBoundaryMatrix(ripsComplex)
   Reducced_BoundaryMatrix = reduceBoundaryMatrix(BoundaryMatrix)
   intervals = readIntervals(Reducced_BoundaryMatrix, ripsComplex[1])
